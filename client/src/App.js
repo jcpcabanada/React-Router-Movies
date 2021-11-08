d3a6409d7fc0e744352c36d379ee7731068580e1
jcpcabanada <jcpcabanada@gmail.com>
2021-11-08 14:17:50 -0800

    first
    Took 1 minute

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 
-export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+export default function App() {
+    const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+    const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+    useEffect(() => {
+        const getMovies = () => {
+            axios
+                .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+                .then(response => {
+                    // Study this response with a breakpoint or log statements
+                    // and set the response data as the 'movieList' slice of state
+                })
+                .catch(error => {
+                    console.error('Server Error', error);
+                });
+        }
+        getMovies();
+    }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+    const addToSavedList = id => {
+        // This is stretch. Prevent the same movie from being "saved" more than once
+    };
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
+    return (
+        <div>
+            <SavedList list={[ /* This is stretch */]}/>
 
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+            <div>Replace this Div with your Routes</div>
+        </div>
+    );
 }
